9a6b447e88ea449a83fdea111975297de23ce425
Duvier-Reinosa <duvierreinosa@gmail.com>
2023-05-18 16:22:09 -0500

    init project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # backend
+
+# requisitos
+# node >= v16.16.0
+
+# Como iniciarlo
+## Para compilar:
+## npm run build 
+
+## Para Levantar el servidor
+## npm run dev
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+const bodyParser = require('body-parser')
+
+import express, { application, Application } from "express";
+
+import cors from "cors";
+
+
+const corsOptions={
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200
+}
+
+class Server {
+    public app: Application;
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(cors(corsOptions));
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json({ limit: "50mb" }));
+        this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+    }
+
+    routes(): void {
+        this.app.get('/', (req, res) => {
+            res.send('Hello World');
+        });
+    }
+
+
+    start(): void {
+        const server = this.app.listen(this.app.get('port'), () => {
+            console.log('Server corriendo en', this.app.get('port'));
+        });
+    }
+}
+
+const server = new Server();
+server.start();
